5bbccb3b760452d5990c89d62bdb555cd147ed02
Yevgen Ponomarenko <yevgen.ponomarenko@gmail.com>
2019-12-06 16:06:32 +0200

    feat: code polishing

--- stacks/admin-api/lib/admin-api-stack.ts
+++ stacks/admin-api/lib/admin-api-stack.ts
@@ -1,10 +1,11 @@
-import cdk = require('@aws-cdk/core');
-import taxonomy_service = require('../lib/taxonomy_service')
+import cdk = require("@aws-cdk/core");
+import taxonomy_service = require("../lib/taxonomy_service");
 
 export class AdminApiStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // tslint:disable-next-line: no-unused-expression
     new taxonomy_service.TaxonomyService(this, "Taxonomy");
   }
 }
--- stacks/admin-api/lib/taxonomy_service.ts
+++ stacks/admin-api/lib/taxonomy_service.ts
@@ -2,7 +2,7 @@ import apigateway = require("@aws-cdk/aws-apigateway");
 import lambda = require("@aws-cdk/aws-lambda");
 import s3 = require("@aws-cdk/aws-s3");
 import cognito = require("@aws-cdk/aws-cognito");
-import { Construct, Aws, RemovalPolicy} from '@aws-cdk/core';
+import { Construct, Aws } from "@aws-cdk/core";
 import { CfnAuthorizer, AuthorizationType } from "@aws-cdk/aws-apigateway";
 import { UserPool, UserPoolClient, AuthFlow, CfnUserPoolClient } from "@aws-cdk/aws-cognito";
 
@@ -10,9 +10,9 @@ export class TaxonomyService extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    const bucket = new s3.Bucket(this, "Taxonomy", {});
+    const bucket: s3.Bucket = new s3.Bucket(this, "Taxonomy", {});
 
-    const handler = new lambda.Function(this, "Handler", {
+    const handler: lambda.Function = new lambda.Function(this, "Handler", {
       runtime: lambda.Runtime.NODEJS_10_X,
       code: lambda.Code.fromAsset("lambdas"),
       handler: "taxonomy.handler",
@@ -21,48 +21,53 @@ export class TaxonomyService extends Construct {
       }
     });
 
-    bucket.grantReadWrite(handler); // was: handler.role);    
+    bucket.grantReadWrite(handler);
 
-    const userPool = new UserPool(this, "Moderators", {});
-    const userPoolClient = new UserPoolClient(this, 'ModeratorsClient',
+    const userPool: UserPool = new UserPool(this, "Moderators", {});
+    const userPoolClient: UserPoolClient = new UserPoolClient(this, "ModeratorsClient",
       {
-        userPoolClientName: 'spa',
+        userPoolClientName: "spa",
         userPool: userPool,
         enabledAuthFlows: [AuthFlow.USER_PASSWORD],
         generateSecret: true
       });
-    const cfnUserPoolClient = userPoolClient.node.defaultChild as CfnUserPoolClient;
-    cfnUserPoolClient.supportedIdentityProviders = ['COGNITO'];
-    cfnUserPoolClient.callbackUrLs = ['https://localhost:4200/signin-callback.html', 'https://localhost:4200/silent-renew.html'];
+    const cfnUserPoolClient: CfnUserPoolClient = userPoolClient.node.defaultChild as CfnUserPoolClient;
+    cfnUserPoolClient.supportedIdentityProviders = ["COGNITO"];
+    cfnUserPoolClient.callbackUrLs = [
+      "https://localhost:4200/signin-callback.html",
+      "https://localhost:4200/silent-renew.html"
+    ];
     cfnUserPoolClient.allowedOAuthFlowsUserPoolClient = true;
-    cfnUserPoolClient.allowedOAuthFlows = ['code'];
-    cfnUserPoolClient.allowedOAuthScopes = ['openid'];
-    
-    const api = new apigateway.RestApi(this, "Api", {
+    cfnUserPoolClient.allowedOAuthFlows = ["code"];
+    cfnUserPoolClient.allowedOAuthScopes = ["openid"];
+
+    const api: apigateway.RestApi = new apigateway.RestApi(this, "Api", {
       restApiName: "Taxonomy Service",
       description: "This service serves taxonomy."
-    });       
+    });
 
-    const region = Aws.REGION;
-    const account = Aws.ACCOUNT_ID;
-    const cognitoArn = `arn:aws:cognito-idp:${region}:${account}:userpool/${userPool.userPoolId}`;
+    const region: string = Aws.REGION;
+    const account: string = Aws.ACCOUNT_ID;
+    const cognitoArn: string = `arn:aws:cognito-idp:${region}:${account}:userpool/${userPool.userPoolId}`;
 
-    const auth = new CfnAuthorizer(this, 'APIGatewayAuthorizer', {
-      name: 'customer-authorizer',
-      identitySource: 'method.request.header.Authorization',
+    const auth: CfnAuthorizer = new CfnAuthorizer(this, "APIGatewayAuthorizer", {
+      name: "customer-authorizer",
+      identitySource: "method.request.header.Authorization",
       providerArns: [cognitoArn],
       restApiId: api.restApiId,
       type: AuthorizationType.COGNITO
     });
 
-    const getTaxonomyIntegration = new apigateway.LambdaIntegration(handler, {
-      requestTemplates: { "application/json": '{ "statusCode": "200" }' }
+    const getTaxonomyIntegration: apigateway.LambdaIntegration = new apigateway.LambdaIntegration(handler, {
+      requestTemplates: { "application/json": "{ \"statusCode\": \"200\" }" }
     });
 
-    const taxonomyResource = api.root.addResource('taxonomy');
+    const taxonomyResource:apigateway.Resource = api.root.addResource("taxonomy");
+
+    // method GET
     taxonomyResource.addMethod("GET", getTaxonomyIntegration, {
       authorizationType: AuthorizationType.COGNITO,
       authorizer: { authorizerId: auth.ref }
-    }); // GET /    
+    });
   }
 }
