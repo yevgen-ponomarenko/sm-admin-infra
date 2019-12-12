44e86b7ef2366a3e02d257206f537c83c3d2f2de
Yevgen Ponomarenko <yevgen.ponomarenko@gmail.com>
2019-12-12 19:21:46 +0200

    feat: adjust to review service requirements

--- stacks/admin-api/lib/review_service.ts
+++ stacks/admin-api/lib/review_service.ts
@@ -5,16 +5,16 @@ import { Construct, Aws } from "@aws-cdk/core";
 import { CfnAuthorizer, AuthorizationType } from "@aws-cdk/aws-apigateway";
 import { UserPool, CfnUserPoolUser, CfnUserPoolDomain, UserPoolClient, AuthFlow, CfnUserPoolClient } from "@aws-cdk/aws-cognito";
 
-export class TaxonomyService extends Construct {
+export class ReviewService extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    const bucket: s3.Bucket = new s3.Bucket(this, "Taxonomy", {});
+    const bucket: s3.Bucket = new s3.Bucket(this, "Review", {});
 
     const handler: lambda.Function = new lambda.Function(this, "Handler", {
       runtime: lambda.Runtime.NODEJS_10_X,
       code: lambda.Code.fromAsset("lambdas"),
-      handler: "taxonomy.handler",
+      handler: "review.handler",
       environment: {
         BUCKET: bucket.bucketName
       }
@@ -22,22 +22,24 @@ export class TaxonomyService extends Construct {
 
     bucket.grantReadWrite(handler);
 
-    const userPool: UserPool = new UserPool(this, "Moderators", {});    
-    this.addUser(userPool.userPoolId, "yevgen-ponomarenko", "yevgen.ponomarenko@gmail.com");
+    const userPool: UserPool = new UserPool(this, "Reviewers", {
+      userPoolName: "Reviewers"
+    });
 
-    const userPoolClient: UserPoolClient = new UserPoolClient(this, "ModeratorsClient",
-      {
-        userPoolClientName: "LocalDev",
-        userPool: userPool,
-        enabledAuthFlows: [AuthFlow.USER_PASSWORD],
-        generateSecret: false
-      });
+    this.addUser(userPool.userPoolId, "yevgen-ponomarenko", "yevgen.ponomarenko@gmail.com");   
 
-    const userPoolDomain: CfnUserPoolDomain = new CfnUserPoolDomain(this, "ModeratorsClientDomain", {
+    const userPoolDomain: CfnUserPoolDomain = new CfnUserPoolDomain(this, "ReviewClientDomain", {
       userPoolId: userPool.userPoolId,
       domain: "staging-yevgen-ponomarenko"
     });
 
+    const userPoolClient: UserPoolClient = new UserPoolClient(this, "ReviewClient",
+      {
+        userPool: userPool,
+        userPoolClientName: "Reviewers",
+        enabledAuthFlows: [AuthFlow.USER_PASSWORD],
+        generateSecret: false
+      });
     const cfnUserPoolClient: CfnUserPoolClient = userPoolClient.node.defaultChild as CfnUserPoolClient;
     cfnUserPoolClient.supportedIdentityProviders = ["COGNITO"];
     cfnUserPoolClient.callbackUrLs = [
@@ -48,16 +50,16 @@ export class TaxonomyService extends Construct {
     cfnUserPoolClient.allowedOAuthFlows = ["code"];
     cfnUserPoolClient.allowedOAuthScopes = ["phone", "email", "openid", "aws.cognito.signin.user.admin", "profile"];
 
-    const api: apigateway.RestApi = new apigateway.RestApi(this, "Api", {
-      restApiName: "Taxonomy Service",
-      description: "This service serves taxonomy."
+    const api: apigateway.RestApi = new apigateway.RestApi(this, "ReviewApi", {
+      restApiName: "Review Service",
+      description: "This service serves annotations review process."
     });
 
     const region: string = Aws.REGION;
     const account: string = Aws.ACCOUNT_ID;
     const cognitoArn: string = `arn:aws:cognito-idp:${region}:${account}:userpool/${userPool.userPoolId}`;
 
-    const auth: CfnAuthorizer = new CfnAuthorizer(this, "APIGatewayAuthorizer", {
+    const auth: CfnAuthorizer = new CfnAuthorizer(this, "ReviewAPIGatewayAuthorizer", {
       name: "customer-authorizer",
       identitySource: "method.request.header.Authorization",
       providerArns: [cognitoArn],
@@ -65,17 +67,16 @@ export class TaxonomyService extends Construct {
       type: AuthorizationType.COGNITO
     });
 
-    const getTaxonomyIntegration: apigateway.LambdaIntegration = new apigateway.LambdaIntegration(handler, {
+    const getReviewIntegration: apigateway.LambdaIntegration = new apigateway.LambdaIntegration(handler, {
       requestTemplates: { "application/json": "{ \"statusCode\": \"200\" }" }
     });
 
-    const taxonomyResource:apigateway.Resource = api.root.addResource("taxonomy");
-
-    // method GET
-    taxonomyResource.addMethod("GET", getTaxonomyIntegration, {
-      authorizationType: AuthorizationType.COGNITO,
+    const authOptions: apigateway.MethodOptions = {
+      authorizationType: auth.type as apigateway.AuthorizationType,
       authorizer: { authorizerId: auth.ref }
-    });
+    };
+
+    this.addResource(api.root, "review-results", "GET", getReviewIntegration, authOptions);
   }
 
   private addUser(userPoolId: string, userName: string, email: string): CfnUserPoolUser {
@@ -84,5 +85,16 @@ export class TaxonomyService extends Construct {
       username: userName,
       userAttributes: [{ name: "email", value: email }]
     });
+  }
+
+  private addResource(
+    parentResource: apigateway.IResource,
+    subResourceName: string,
+    subResourceMethod: string,
+    lambdaIntegration: apigateway.LambdaIntegration,
+    opts: apigateway.MethodOptions): apigateway.IResource {
+      const subResource:apigateway.Resource = parentResource.addResource(subResourceName);
+      subResource.addMethod(subResourceMethod, lambdaIntegration, opts);
+      return subResource;
   }
 }
